508890ea7c0030bae237b83eee965fee809666e9
Rat-without-hat <antonryzhov2007@gmail.com>
2026-04-24 12:09:04 +0300

    Update __site-1.1__.py

--- __site-1.1__.py
+++ __site-1.1__.py
@@ -54,12 +54,6 @@ def reset_features():
     st.session_state.E_section = None
     st.session_state.even_feature = None
 
-if 'start_size' not in st.session_state:
-    st.session_state.start_size = 1
-
-if 'end_size' not in st.session_state:
-    st.session_state.end_size = 1
-
 def convert_for_saving(df):
     return df.to_csv(index=False).encode("utf-8")
 
@@ -191,8 +185,6 @@ with main_col_1:
 
     if start_tab and end_tab and step_tab:
         x_func_val = np.arange(start_tab, end_tab, step_tab, dtype=np.float64)
-        st.session_state.start_size = start_tab
-        st.session_state.end_size = end_tab
 
     if accuracy_val:
         len_accuracy = int(len(str(accuracy_val)[2:]))
@@ -393,8 +385,8 @@ graph_fig.set_size_inches(15, 13.5)
 if st.session_state.button_grid:
     ax.grid()
 
-ax.set_xlim(st.session_state.start_size * st.session_state.coef_size, st.session_state.end_size * st.session_state.coef_size)
-ax.set_ylim(st.session_state.start_size * st.session_state.coef_size, st.session_state.end_size * st.session_state.coef_size)
+ax.set_xlim(10 * st.session_state.coef_size, 10 * st.session_state.coef_size)
+ax.set_ylim(10 * st.session_state.coef_size, 10 * st.session_state.coef_size)
 
 ax.set_facecolor((.0588, .0667, .0863))
 
